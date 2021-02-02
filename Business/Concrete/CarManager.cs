6e99a8beefd96924e46e4b964803f0497701f641
Nazile-Tag <tagnazile@gmail.com>
2021-02-02 23:20:36 +0300

    Add project files.

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,24 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        ICarDal _carsDal;
+
+        public CarManager(ICarDal carsDal)
+        {
+            _carsDal = carsDal;
+        }
+
+        public List<Car> GetAll()
+        {
+            return _carsDal.GetAll();
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,58 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car> {
+
+                new Car{ CarId=1, BrandId=20, ColorId=253, ModelYear=2020, DailyPrice=300, Description="Mercedes A180" },
+                new Car{ CarId=2, BrandId=20, ColorId=254, ModelYear=2020, DailyPrice=350, Description="Mercedes CLA AMG" },
+                new Car{ CarId=3, BrandId=21, ColorId=255, ModelYear=2021, DailyPrice=380, Description="Mercedes CLA 200" },
+                new Car{ CarId=4, BrandId=21, ColorId=256, ModelYear=2021, DailyPrice=390, Description="Mercedes CLS" },
+                new Car{ CarId=5, BrandId=22, ColorId=257, ModelYear=2021, DailyPrice=400, Description="Mercedes GLB" },
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetById(int categoryId)
+        {
+            return _cars.Where(c => c.CategoryId == categoryId).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+        }
+    }
+
+}
